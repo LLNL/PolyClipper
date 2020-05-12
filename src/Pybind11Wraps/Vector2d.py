62cf889648f6773ab393b18eda79b33692f24083
Mike Owen <mikeowen@llnl.gov>
2020-05-12 14:33:36 -0700

    Fleshing out Vector2d.

--- src/Pybind11Wraps/Vector2d.py
+++ src/Pybind11Wraps/Vector2d.py
@@ -1,3 +1,5 @@
+from PYB11Generator import *
+
 class Vector2d:
     "The PolyClipper (x,y) geometric vector type"
 
@@ -11,3 +13,56 @@ class Vector2d:
                 X = "double",
                 Y = "double"):
         "Construct with (x,y) values"
+
+    #---------------------------------------------------------------------------
+    # Operators
+    #---------------------------------------------------------------------------
+    def __eq__(self):
+        return
+
+    def __imul__(self, rhs="double()"):
+        return
+
+    def __idiv__(self, rhs="double()"):
+        return
+
+    def __iadd__(self):
+        return
+
+    def __isub__(self):
+        return
+
+    def __mul__(self, rhs="double()"):
+        return
+
+    def __div__(self, rhs="double()"):
+        return
+
+    def __add__(self):
+        return
+
+    def __sub__(self):
+        return
+
+    #---------------------------------------------------------------------------
+    # Methods
+    #---------------------------------------------------------------------------
+    @PYB11const
+    def dot(self, rhs="const Vector2d&"):
+        "Return the dot product"
+        return "double"
+
+    @PYB11const
+    def cross(self, rhs="const Vector2d&"):
+        "Return the z-value of the cross product as a scalar"
+        return "double"
+
+    @PYB11const
+    def magnitude(self):
+        "Return the magnitude of the vector"
+        return "double"
+
+    @PYB11const
+    def magnitude2(self):
+        "Return the square of the magnitude of the vector"
+        return "double"
